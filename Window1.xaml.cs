57e0dd5ee503e057b70e780b35bd051e5d31d735
Do3t <131492598+Filip3T@users.noreply.github.com>
2025-04-09 13:00:59 +0200

    Add files via upload

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,45 +20,77 @@ namespace WpfApp1
     /// </summary>
     class NewItem
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string LastName { get; set; }
         public string Pesel { get; set; }
+        public string Name { get; set; }
+        public string SecName { get; set; }
+        public string LastName { get; set; }
+        public string Date { get; set; }
+        public string Phone { get; set; }
+        public string Adres { get; set; }
+        public string Local { get; set; }
+        public string Post { get; set; }
 
-        public NewItem(string n_Name, string n_LastName, string n_Pesel)
+        public NewItem(string n_Pesel, string n_Name, string n_SecName, string n_LastName, string n_Date, string n_Phone, string n_Adres,  string n_Local, string n_Post)
         {
-            var random = new Random();
-            ID = random.Next();
-            Name = n_Name;
-            LastName = n_LastName;
             Pesel = n_Pesel;
+            Name = n_Name;
+            SecName = n_SecName;
+            LastName = n_LastName;
+            Date = n_Date;
+            Phone = n_Phone;
+            Adres = n_Adres;
+            Local = n_Local;
+            Post = n_Post;
         }
     }
     public partial class Window1 : Window
     {
-        NewItem item = new NewItem("", "", "");
+        
         public Window1()
         {
             InitializeComponent();
-            ID.Text = $"{item.ID}";
+        }
+
+        public bool empty_check()
+        {
+            bool empty = false;
+            if (Pesel.Text == "") { empty = true; Pesel.Background = Brushes.Red; }
+            if (Name.Text == "") { empty = true; Name.Background = Brushes.Red; }
+            if (LastName.Text == "") { empty = true; LastName.Background = Brushes.Red; }
+            if (Date.Text == "") { empty = true; Date.Background = Brushes.Red; }
+            if (Adres.Text == "") { empty = true; Adres.Background = Brushes.Red; }
+            if (Local.Text == "") { empty = true; Local.Background = Brushes.Red; }
+            if (Post.Text == "") { empty = true; Post.Background = Brushes.Red; }
+
+            return empty;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            item.Name = Name.Text;
-            item.LastName = LastName.Text;
-            item.Pesel = Pesel.Text;
-            MainWindow mainWindow = Owner as MainWindow;
+            bool empty = empty_check();
 
-            mainWindow.Lista.Items.Add(new { m_nID = item.ID, m_strName = item.Name, m_strLastName = item.LastName, m_strPesel = item.Pesel });
+            if (!empty)
+            {
+                NewItem item = new NewItem(Pesel.Text, Name.Text, SecName.Text, LastName.Text, Date.Text, Phone.Text, Adres.Text, Local.Text, Post.Text);
+                MainWindow mainWindow = Owner as MainWindow;
 
-            this.Close();
+                mainWindow.Lista.Items.Add(new {m_strName = item.Name, m_strLastName = item.LastName, m_strPesel = item.Pesel, m_strSecName = item.SecName,
+                m_strDate = item.Date, m_strPhone = item.Phone, m_strAdres = item.Adres, m_strLocal = item.Local, m_strPost = item.Post});
+                this.Close();
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            var Result = MessageBox.Show("Jesteś pweiwn?", "Anuluj" , MessageBoxButton.YesNo, MessageBoxImage.Question);
-            if (Result == MessageBoxResult.Yes)
+            bool empty = empty_check();
+            if(!empty)
+            {
+                var Result = MessageBox.Show("Jesteś pweiwn?", "Anuluj", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (Result == MessageBoxResult.Yes)
+                {
+                    this.Close();
+                }
+            } else
             {
                 this.Close();
             }
