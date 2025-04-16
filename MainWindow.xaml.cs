a53220b380e22ec69a61b7525d7a1b66cfff0152
Do3t <131492598+Filip3T@users.noreply.github.com>
2025-04-16 12:59:09 +0200

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,13 +9,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Collections.Generic;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-   
+    class NewItem
+    {
+        public string NI_Pesel { get; set; }
+        public string NI_Name { get; set; }
+        public string NI_SecName { get; set; }
+        public string NI_LastName { get; set; }
+        public string NI_Date { get; set; }
+        public string NI_Phone { get; set; }
+        public string NI_Adres { get; set; }
+        public string NI_Local { get; set; }
+        public string NI_Post { get; set; }
+
+        public NewItem(string n_Pesel, string n_Name, string n_SecName, string n_LastName, string n_Date, string n_Phone, string n_Adres, string n_Local, string n_Post)
+        {
+            NI_Pesel = n_Pesel;
+            NI_Name = n_Name;
+            NI_SecName = n_SecName;
+            NI_LastName = n_LastName;
+            NI_Date = n_Date;
+            NI_Phone = n_Phone;
+            NI_Adres = n_Adres;
+            NI_Local = n_Local;
+            NI_Post = n_Post;
+        }
+    }
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -28,6 +58,83 @@ namespace WpfApp1
             window.Owner = this;
 
             window.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            while (Lista.SelectedItems.Count > 0)
+            {
+                Lista.Items.Remove(Lista.SelectedItems[0]);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            openFileDialog.Title = "Otwórz plik CSV";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Lista.Items.Clear();
+                string filePath = openFileDialog.FileName;
+                int selectedFilterIndex = openFileDialog.FilterIndex;
+                string delimiter = ";";
+                if (selectedFilterIndex == 1)
+                {
+                    delimiter = ",";
+                }
+                Encoding encoding = Encoding.UTF8;
+                if (File.Exists(filePath))
+                {
+                    var lines = File.ReadAllLines(filePath, encoding);
+                    foreach (var line in lines)
+                    {
+                        string[] columns = line.Split(delimiter);
+                        if (columns != null)
+                        {
+                            Lista.Items.Add(new
+                            {
+                                m_strName = columns[1],
+                                m_strLastName = columns[3],
+                                m_strPesel = columns[0],
+                                m_strSecName = columns[2],
+                                m_strDate = columns[4],
+                                m_strPhone = columns[5],
+                                m_strAdres = columns[6],
+                                m_strLocal = columns[7],
+                                m_strPost = columns[8]
+                            });
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            saveFileDialog.Title = "Zapisz jako plik CSV";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                string delimiter = ";";
+                if (saveFileDialog.FilterIndex == 1)
+                {
+                    delimiter = ",";
+                }
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (NewItem item in Lista.Items)
+                    {
+                        var row = $"{item.NI_Pesel}{delimiter}{item.NI_Name}" +
+                        $"{delimiter}{item.NI_SecName}{delimiter}{item.NI_LastName}" +
+                        $"{delimiter}{item.NI_Date}{delimiter}{item.NI_Phone}" +
+                        $"{delimiter}{item.NI_Adres}{delimiter}{item.NI_Local}{delimiter}{item.NI_Post}";
+                        writer.WriteLine(row);
+                    }
+                }
+            }
         }
     }
 }
--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -18,37 +18,88 @@ namespace WpfApp1
     /// <summary>
     /// Logika interakcji dla klasy Window1.xaml
     /// </summary>
-    class NewItem
-    {
-        public string Pesel { get; set; }
-        public string Name { get; set; }
-        public string SecName { get; set; }
-        public string LastName { get; set; }
-        public string Date { get; set; }
-        public string Phone { get; set; }
-        public string Adres { get; set; }
-        public string Local { get; set; }
-        public string Post { get; set; }
-
-        public NewItem(string n_Pesel, string n_Name, string n_SecName, string n_LastName, string n_Date, string n_Phone, string n_Adres,  string n_Local, string n_Post)
-        {
-            Pesel = n_Pesel;
-            Name = n_Name;
-            SecName = n_SecName;
-            LastName = n_LastName;
-            Date = n_Date;
-            Phone = n_Phone;
-            Adres = n_Adres;
-            Local = n_Local;
-            Post = n_Post;
-        }
-    }
+    
     public partial class Window1 : Window
     {
         
         public Window1()
         {
             InitializeComponent();
+        }
+
+        static int SumaKontrolna(string pesel)
+        {
+            int kontrolna = 0;
+            int[] wagi = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            for (int i = 0; i < 10; i++)
+            {
+                kontrolna += int.Parse(pesel[i].ToString()) * wagi[i];
+            }
+            kontrolna %= 10;
+            kontrolna = 10 - kontrolna;
+            kontrolna %= 10;
+            return kontrolna;
+        }
+        static bool CzyPoprawnyPesel(string pesel, ref int rok, ref int miesiac, ref int dzien)
+        {
+            bool correct = true;
+
+            if (pesel.Length != 11)
+            {
+                correct = false;
+            }
+            else
+            {
+                char[] pesel_rok_ch = [pesel[0], pesel[1]];
+                int pesel_rok_int = int.Parse(new string(pesel_rok_ch.ToArray()));
+                if (rok % 100 != pesel_rok_int)
+                {
+                    correct = false;
+                }
+                else
+                {
+                    int miesiac_tmp = miesiac;
+                    int stulecie = ((rok % 1000) - (rok % 100)) / 100;
+                    switch (stulecie)
+                    {
+                        case 8:
+                            miesiac_tmp += 80;
+                            break;
+                        case 0:
+                            miesiac_tmp += 20;
+                            break;
+                        case 1:
+                            miesiac_tmp += 40;
+                            break;
+                        case 2:
+                            miesiac_tmp += 60;
+                            break;
+                    }
+                    char[] pesel_miesiac_ch = [pesel[2], pesel[3]];
+                    int pesel_miesiac_int = int.Parse(new string(pesel_miesiac_ch.ToArray()));
+                    if (miesiac_tmp != pesel_miesiac_int)
+                    {
+                        correct = false;
+                    }
+                    else
+                    {
+                        char[] pesel_dzien_ch = [pesel[4], pesel[5]];
+                        int pesel_dzien_int = int.Parse(new string(pesel_dzien_ch.ToArray()));
+                        if (dzien != pesel_dzien_int)
+                        {
+                            correct = false;
+                        }
+                        else
+                        {
+                            if (int.Parse(pesel[10].ToString()) != SumaKontrolna(pesel))
+                            {
+                                correct = false;
+                            }
+                        }
+                    }
+                }
+            }
+            return correct;
         }
 
         public bool empty_check()
@@ -71,12 +122,39 @@ namespace WpfApp1
 
             if (!empty)
             {
-                NewItem item = new NewItem(Pesel.Text, Name.Text, SecName.Text, LastName.Text, Date.Text, Phone.Text, Adres.Text, Local.Text, Post.Text);
-                MainWindow mainWindow = Owner as MainWindow;
+                string data = Date.SelectedDate.ToString().Split()[0];
+                string Srok, Smiesiac, Sdzien;
+                Srok = data.Split(".")[2];
+                Smiesiac = data.Split(".")[1];
+                Sdzien = data.Split(".")[0];
+                int rok, miesiac, dzien;
+                int.TryParse(Srok, out rok);
+                int.TryParse(Smiesiac, out miesiac);
+                int.TryParse(Sdzien, out dzien);
 
-                mainWindow.Lista.Items.Add(new {m_strName = item.Name, m_strLastName = item.LastName, m_strPesel = item.Pesel, m_strSecName = item.SecName,
-                m_strDate = item.Date, m_strPhone = item.Phone, m_strAdres = item.Adres, m_strLocal = item.Local, m_strPost = item.Post});
-                this.Close();
+
+                if (CzyPoprawnyPesel(Pesel.Text, ref rok, ref miesiac, ref dzien))
+                {
+                    NewItem item = new NewItem(Pesel.Text, Name.Text, SecName.Text, LastName.Text, data, Phone.Text, Adres.Text, Local.Text, Post.Text);
+                    MainWindow mainWindow = Owner as MainWindow;
+
+                    mainWindow.Lista.Items.Add(new
+                    {
+                        m_strName = item.NI_Name,
+                        m_strLastName = item.NI_LastName,
+                        m_strPesel = item.NI_Pesel,
+                        m_strSecName = item.NI_SecName,
+                        m_strDate = item.NI_Date,
+                        m_strPhone = item.NI_Phone,
+                        m_strAdres = item.NI_Adres,
+                        m_strLocal = item.NI_Local,
+                        m_strPost = item.NI_Post
+                    });
+                    this.Close();
+                } else
+                {
+                    Pesel.Background = Brushes.Red;
+                }
             }
         }
 
